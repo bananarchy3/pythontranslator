178179e4c2883741457f57a3302a1d46bde4f249
Tom Russell <51247374+bananarchy3@users.noreply.github.com>
2021-04-27 12:09:15 -0700

    Updated output_lang user input

--- japanesetranslator.py
+++ japanesetranslator.py
@@ -3,14 +3,16 @@ from translate import Translator
 
 os.chdir(r'C:\Users\savew\Desktop')
 
-translator = Translator(to_lang='ja')
+output_lang = input('Enter output language ISO code. A full list is available at '
+                    'https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes')
+translator = Translator(to_lang=output_lang)
 
 try:
-    with open('test.txt', mode='r', encoding="utf-8") as my_file:
+    with open('input.txt', mode='r', encoding="utf-8") as my_file:
         text = my_file.read()
         translation = str(translator.translate(text))
         # Creates a new output file for translated text.
-        with open('ja-test.txt', mode='w', encoding="utf-8") as my_file2:
+        with open('output.txt', mode='w', encoding="utf-8") as my_file2:
             my_file2.write(translation)
 except FileNotFoundError as e:
-    print('File not found, check your file path')
+    print('File not found, check your file path or file name')
